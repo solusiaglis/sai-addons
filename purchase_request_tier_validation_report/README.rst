0e25dc95f93c701b82d272d6fc92939c8f665efc
hitrosol <ppakpahan@solusiaglis.co.id>
2025-02-26 08:25:58 +0700

    adding the tier validation section on the purchase_request_as_tor report

---
+++ purchase_request_tier_validation_report/README.rst
@@ -0,0 +1,21 @@
+=======================================
+Purchase Request Tier Validation Report
+=======================================
+
+This module extends the functionality to print the tier validation section on the purchase_request_as_tor report
+
+**Table of contents**
+
+.. contents::
+   :local:
+
+Authors
+~~~~~~~
+
+* PT Solusi Aglis Indonesia
+
+Contributors
+~~~~~~~~~~~~
+
+* Panca Putra Pakpahan <ppakpahan@solusiaglis.co.id>
+
---
+++ purchase_request_tier_validation_report/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 PT Solusi Aglis Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Purchase Request Validation Report",
+    "version": "14.0.1.0.0",
+    "author": "PT Solusi Aglis Indonesia",
+    "category": "Purchase",
+    "summary": "Add validation history to purchase request report",
+    "depends": ["purchase_request_tier_validation"],
+    "website": "https://github.com/solusiaglis/sai-addons",
+    "data": [
+        "reports/purchase_request_report_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
